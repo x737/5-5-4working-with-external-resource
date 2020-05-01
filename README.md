185038f19c5919c294f61817ce1e6e3fbd7aaead
x737 <han9yolanda@gmail.com>
2020-05-01 23:22:59 +0000

    1st commit

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ The DOM allow us the write Data to the page by setting the innerHTML
 + modify open() method, add baseURL + type. this will append the base URL with the type that we're parsing in, so that could be people, films, vehicles, or species.
 + modify HTML page accordingly
 
-## Unpacking the data onto the DOM
+## Unpacking the data onto the DOM (main1.js)
 
 the combination of JSON data and the DOM allows us to present our data to our users
 + to unpack the data, firstly access them by .dir() method
---
+++ main2.js
@@ -0,0 +1,40 @@
+const baseURL = "https://ci-swapi.herokuapp.com/api/";
+
+function getData(type, cb) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            cb(JSON.parse(this.responseText));
+        }
+    };
+
+    xhr.open("GET", baseURL + type + "/");
+    xhr.send();
+}
+
+function getTableHeaders(obj) {
+    var tableHeaders = [];
+
+    Object.keys(obj).forEach(function(key) {
+        tableHeaders.push(`<td>${key}</td>`)
+    });
+
+    return `<tr>${tableHeaders}</tr>`;
+}
+
+function writeToDocument(type) {
+    var el = document.getElementById("data");
+    el.innerHTML = "";
+
+    getData(type, function(data) {
+        data = data.results;
+        var tableHeaders = getTableHeaders(data[0]);
+
+        data.forEach(function(item) {
+            // el.innerHTML += "<p>" + item.name + "</p>";
+        });
+
+        el.innerHTML = `<table>${tableHeaders}</table>`;
+    });
+}
